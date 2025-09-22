2d8a077162a9237f81fd30bc70a49d16935e0eab
duy4nt <singhduyant@gmail.com>
2025-09-22 13:54:49 +0000

    made the exception handle more sophisticated

--- number.py
+++ number.py
@@ -1,5 +1,10 @@
-try:
-    x = int(input("Whats x?"))
-    print(f"x is {x}")
-except ValueError:
-    print("x is not a number")
+while True:
+    try:
+        x = int(input("Whats x?"))
+        print(f"x is {x}")
+    except ValueError:
+        print("x is not a number")
+    else:
+        break
+
+print(f"x is {x}")
