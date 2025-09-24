71d6b7be3d3abf940571e3f42acac563b579737a
duy4nt <singhduyant@gmail.com>
2025-09-24 07:11:08 +0000

    Working with some APIs

--- itunes.py
+++ itunes.py
@@ -1,4 +1,13 @@
 import requests
 import sys
+import json
 
-if len
+if len(sys.argv) != 2:
+    sys.exit()
+
+response =requests.get("https://itunes.apple.com/search?limit=5&term="+sys.argv[1])
+
+#print(json.dumps(response.json(), indent = 2))
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
