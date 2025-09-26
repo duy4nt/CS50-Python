cbc189308b21fbc669287e80d7d51b20681ec017
duy4nt <singhduyant@gmail.com>
2025-09-26 14:02:52 +0000

    Spread the functionality of the programs into programs

--- oop/student.py
+++ oop/student.py
@@ -1,9 +1,17 @@
+class Students:
+    ...
+
+
+
 def main():
     name, house = get_student()
+    if name == "Padma":
+        house = "RavenClaw"
     print(f"{name} from {house}")
 
 def get_student():
-    return get_name(), get_house()
+    return [get_name(), get_house()]
+    # The above line returns a list rather than default Tuple, but it is immutable
 
 def get_name():
     return input("Name: ")
